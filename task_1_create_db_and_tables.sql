53ebab66fdf1bf472c5308dd31023b8798db3003
Bohdan Bodnar <iloveyoulviv@gmail.com>
2021-03-01 20:05:37 +0200

    Fix all troubles in PR comments

--- task_1_create_db_and_tables.sql
+++ task_1_create_db_and_tables.sql
@@ -1,55 +1,48 @@
 -- create DB
-CREATE SCHEMA `company_employee` DEFAULT CHARACTER SET utf8;
-
-USE company_employee;
+CREATE SCHEMA `company_employee`;
 
 -- create table Region
-CREATE TABLE `region`
+create table region
 (
-    `id`   bigint      NOT NULL AUTO_INCREMENT,
-    `name` varchar(45) NOT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `name_UNIQUE` (`name`)
-)
+    id   bigserial
+        constraint region_pk
+            primary key,
+    name varchar not null
+);
 
 -- create table Company
-CREATE TABLE `company`
+create table company
 (
-    `id`            BIGINT      NOT NULL AUTO_INCREMENT,
-    `name`          VARCHAR(45) NOT NULL,
-    `address`       VARCHAR(45) NULL,
-    `phone`         VARCHAR(45) NULL,
-    `region`        BIGINT      NOT NULL,
-    `country`       VARCHAR(45) NULL,
-    `city`          VARCHAR(45) NULL,
-    `active`        TINYINT NULL,
-    `business_area` VARCHAR(45) NULL,
-    `offices`       INT NULL,
-    `employees`     INT NULL,
-    PRIMARY KEY (`id`),
-    INDEX           `region_id_idx` (`region` ASC) VISIBLE,
-    CONSTRAINT `region_id`
-        FOREIGN KEY (`region`)
-            REFERENCES `company_employee`.`region` (`id`)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION
+    id                bigserial
+        constraint company_pk
+            primary key,
+    name              varchar not null,
+    address           varchar,
+    phone             varchar,
+    region_id         bigserial
+        constraint region___fk
+            references region,
+    country           varchar,
+    city              varchar,
+    is_active         boolean,
+    business_area     varchar,
+    count_of_office   int,
+    count_of_employee int
 );
 
 -- create table Employee
-CREATE TABLE `employee`
+create table employee
 (
-    `id`                         BIGINT NOT NULL AUTO_INCREMENT,
-    `first_name`                 VARCHAR(45) NULL,
-    `last_name`                  VARCHAR(45) NULL,
-    `position`                   VARCHAR(45) NULL,
-    `married`                    TINYINT NULL,
-    `number_of_years_in_company` INT NULL,
-    `company`                    BIGINT NOT NULL,
-    PRIMARY KEY (`id`),
-    INDEX                        `company_id_idx` (`company` ASC) VISIBLE,
-    CONSTRAINT `company_id`
-        FOREIGN KEY (`company`)
-            REFERENCES `company_employee`.`company` (`id`)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION
+    id                         bigserial
+        constraint employee_pk
+            primary key,
+    first_name                 varchar,
+    last_name                  varchar,
+    position                   varchar,
+    is_married                 boolean,
+    number_of_years_in_company int,
+    company_id                 bigserial not null
+        constraint company___fk
+            references company
 );
+
