486afbe84c9ff6deb1bd7cf5bd5cb7d8ea6f99ad
Bohdan Bodnar <iloveyoulviv@gmail.com>
2021-03-03 12:04:25 +0200

    feature_3 added spring boot skeleton

---
+++ src/test/java/com/github/bbodnar/companyemployees/CompanyEmployeesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.bbodnar.companyemployees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyEmployeesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
