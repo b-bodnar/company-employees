cc794047acfef9f7364fcaa680635c866892b337
Bohdan Bodnar <iloveyoulviv@gmail.com>
2021-03-03 12:04:25 +0200

    feature_3 added spring boot skeleton

---
+++ src/main/java/com/github/bbodnar/companyemployees/CompanyEmployeesApplication.java
@@ -0,0 +1,13 @@
+package com.github.bbodnar.companyemployees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyEmployeesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompanyEmployeesApplication.class, args);
+    }
+
+}
